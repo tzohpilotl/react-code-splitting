f8585a9fd15b6400195af0bc25284e4f94ae6c1f
Mario Gil <mariogillazaro@gmail.com>
2020-03-31 19:41:50 -0600

    2 - Add a second section with worldwide data

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
 import React from 'react';
 
 function App() {
-  const data = {};
+  const countryData = {};
+  const worldData = {};
 
   return (
-    <section>
-      <div className="container">
+    <div className="container">
+      <section className="section">
         <h1 className="title">COVID-19</h1>
-        <h2 className="subtitle">Cases by country</h2>
-        <p className="content">{JSON.stringify(data)}</p>
-      </div>
-    </section>
+      </section>
+
+      <section className="section">
+        <div className="container">
+          <header>
+            <h2 className="subtitle">Cases by country</h2>
+            <p className="content">Various information about the virus by country</p>
+          </header>
+          <p className="content">{JSON.stringify(countryData)}</p>
+        </div>
+      </section>
+
+      <section className="section">
+        <div className="container">
+          <header>
+            <h2 className="subtitle">World total stat</h2>
+            <p className="content">A summary about the virus worldwide</p>
+          </header>
+          <p className="content">{JSON.stringify(worldData)}</p>
+        </div>
+      </section>
+    </div>
   );
 }
 
